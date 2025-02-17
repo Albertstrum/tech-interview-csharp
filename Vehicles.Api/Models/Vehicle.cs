6e90a3c238f96ef3b04499eb43ff4a3492cefdac
Alastair Scott <alastair@appoly.co.uk>
2025-02-17 15:54:03 +0000

    Add remaining fields to vehicle model (from vehicles.json)

--- Vehicles.Api/Models/Vehicle.cs
+++ Vehicles.Api/Models/Vehicle.cs
@@ -4,13 +4,37 @@ namespace Vehicles.Api.Models
 {
     public class Vehicle
     {
+        [JsonPropertyName("price")]
+        public decimal Price { get; set; }
+
         [JsonPropertyName("make")]
         public string Make { get; set; }
+
         [JsonPropertyName("model")]
         public string Model { get; set; }
+
         [JsonPropertyName("trim")]
         public string Trim { get; set; }
+
         [JsonPropertyName("colour")]
         public string Colour { get; set; }
+
+        [JsonPropertyName("co2_level")]
+        public int Co2Level { get; set; }
+
+        [JsonPropertyName("transmission")]
+        public string Transmission { get; set; }
+
+        [JsonPropertyName("fuel_type")]
+        public string FuelType { get; set; }
+
+        [JsonPropertyName("engine_size")]
+        public int EngineSize { get; set; }
+
+        [JsonPropertyName("date_first_reg")]
+        public DateTime DateFirstReg { get; set; }
+
+        [JsonPropertyName("mileage")]
+        public int Mileage { get; set; }
     }
 }
