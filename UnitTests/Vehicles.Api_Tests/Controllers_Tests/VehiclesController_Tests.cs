ece4912c22ba7198bff330960c1cd2e5321074fb
Alastair Scott <alastair@appoly.co.uk>
2025-02-18 17:04:16 +0000

    Add basic unit tests for controller and repo

---
+++ UnitTests/Vehicles.Api_Tests/Controllers_Tests/VehiclesController_Tests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Vehicles.Api.Controllers;
+using Vehicles.Api.Models;
+using Vehicles.Api.Repositories;
+
+namespace UnitTests.Controllers
+{
+    public class VehiclesController_Tests
+    {
+        private readonly Mock<ILogger<VehiclesController>> _loggerMock;
+        private readonly Mock<IVehiclesRepository> _repositoryMock;
+        private readonly VehiclesController _controller;
+
+        public VehiclesController_Tests()
+        {
+            _loggerMock = new Mock<ILogger<VehiclesController>>();
+            _repositoryMock = new Mock<IVehiclesRepository>();
+            _controller = new VehiclesController(_loggerMock.Object, _repositoryMock.Object);
+        }
+
+        [Fact]
+        public void GetAll_IsCalled_ReturnsOkResultWithAllVehicles()
+        {
+            var testVehicles = GetTestVehicles();
+            _repositoryMock.Setup(repo => repo.GetAll()).Returns(testVehicles);
+
+            var result = _controller.GetAll();
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<List<Vehicle>>(okResult.Value);
+            Assert.Equal(testVehicles.Count, returnValue.Count);
+        }
+
+        [Fact]
+        public void GetByQuery_ColourQuery_ReturnsOkResultWithCorrectVehicles()
+        {
+            var testVehicle = new Vehicle { Colour = "Alpine white" };
+            var testVehicles = GetTestVehicles().FindAll(v => v.Colour == "Alpine white");
+            _repositoryMock.Setup(repo => repo.GetByQuery(testVehicle)).Returns(testVehicles);
+
+            var result = _controller.GetByQuery(testVehicle);
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<List<Vehicle>>(okResult.Value);
+            Assert.Equal(testVehicles.Count, returnValue.Count);
+        }
+
+        private List<Vehicle> GetTestVehicles()
+        {
+            return new List<Vehicle>
+            {
+                new Vehicle { Price = 12999, Make = "BMW", Model = "1 SERIES", Trim = "118d SE 5dr [Nav]", Colour = "Alpine white", Co2Level = 104, Transmission = "Manual", FuelType = "Diesel", EngineSize = 1995, DateFirstReg = new DateTime(2017, 12, 28), Mileage = 11271 },
+                new Vehicle { Price = 14699, Make = "BMW", Model = "1 SERIES", Trim = "118d M Sport 5dr [Nav]", Colour = "Melbourne red", Co2Level = 114, Transmission = "Manual", FuelType = "Diesel", EngineSize = 1995, DateFirstReg = new DateTime(2017, 10, 4), Mileage = 13324 }
+            };
+        }
+    }
+}
---
+++ UnitTests/Vehicles.Api_Tests/Repositories_Tests/VehiclesRepository_Tests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using Vehicles.Api.Models;
+using Vehicles.Api.Repositories;
+
+namespace UnitTests
+{
+    public class VehiclesRepository_Tests
+    {
+        private readonly Mock<ILogger<VehiclesRepository>> _loggerMock;
+        private readonly VehiclesRepository _repository;
+
+        public VehiclesRepository_Tests()
+        {
+            _loggerMock = new Mock<ILogger<VehiclesRepository>>();
+            _repository = new VehiclesRepository(_loggerMock.Object);
+        }
+
+        [Fact]
+        public void GetAll_CallRepo_ReturnAllVehicles()
+        {
+            var expectedVehicles = 780;
+
+            var vehicles = _repository.GetAll();
+
+            Assert.Equal(expectedVehicles, vehicles.Count);
+        }
+
+        [Fact]
+        public void GetByQuery_FilterByMake_ReturnsCorrectVehicles()
+        {
+            var testVehicle = new Vehicle { Make = "BMW" };
+            var expectedVehicles = 82;
+
+            var vehicles = _repository.GetByQuery(testVehicle);
+
+            Assert.Equal(expectedVehicles, vehicles.Count);
+            Assert.True(vehicles.All(v => v.Make == testVehicle.Make));
+        }
+
+        [Fact]
+        public void GetByQuery_MakeIsWrongCase_ReturnsNoMatches()
+        {
+            var testVehicle = new Vehicle { Make = "bmw" };
+            var expectedVehicles = 0;
+
+            var vehicles = _repository.GetByQuery(testVehicle);
+
+            Assert.Equal(expectedVehicles, vehicles.Count);
+        }
+    }
+}
